91578ec516775b0e1374ca92b9dbf8041a35560b
jinnuT <jinnuaz@hotmail.com>
2021-07-25 14:17:47 -0700

    Alignment \t

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 LOG=/tmp/roboshop.log
 
-echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
+echo -e "Installing Nginx\t...\t\e[32mdone\e[0m"
 yum install nginx -y >>$LOG
 echo -e "Enabling Nginx\t\t...\t\e[32mdone\e[0m"
 systemctl enable nginx >>$LOG
