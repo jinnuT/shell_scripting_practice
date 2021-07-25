7f83887f7500ab35b08e67dfa36b188f469747a4
jinnuT <jinnuaz@hotmail.com>
2021-07-25 15:22:07 -0700

    Introduced STAT_CHECK function

---
+++ roboshop/common.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+LOG=/tmp/roboshop.log
+rm -f $LOG
--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,27 +3,25 @@
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
+STAT_CHECK(){
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mdone\e[0m"
+  else
+    echo -e "\e[31mfail\e[0m"
+    exit 1
+  fi
+}
+
 echo -n -e "Installing Nginx\t..."
 yum install nginx -y &>>$LOG
-if [ $? -eq 0 ]; then
-   echo -e "\e[32mdone\e[0m"
-else
-   echo -e "\e[31mfail\e[0m"
-   exit 1
-fi
+STAT_CHECK $?
+
+
 echo -n -e "Enabling Nginx\t\t..."
 systemctl enable nginx &>>$LOG
-if [ $? -eq 0 ]; then
-   echo -e "\e[32mdone\e[0m"
-else
-   echo -e "\e[31mfail\e[0m"
-   exit 1
-fi
+STAT_CHECK $?
+
+
 echo -n -e "Starting Nginx\t\t..."
 systemctl start nginx &>>$LOG
-if [ $? -eq 0 ]; then
-   echo -e "\e[32mdone\e[0m"
-else
-   echo -e "\e[31mfail\e[0m"
-   exit 1
-fi
+STAT_CHECK $?
