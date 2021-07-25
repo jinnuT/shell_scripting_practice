a77177373d97a280988d6c96681551d4fa506ca2
jinnuT <jinnuaz@hotmail.com>
2021-07-25 15:30:19 -0700

    Intro common.sh

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -2,3 +2,16 @@
 
 LOG=/tmp/roboshop.log
 rm -f $LOG
+
+STAT_CHECK(){
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mdone\e[0m"
+  else
+    echo -e "\e[31mfail\e[0m"
+    exit 1
+  fi
+}
+
+PRINT(){
+   echo -n -e "$1\t\t"
+}
--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/bash
 
-LOG=/tmp/roboshop.log
-rm -f $LOG
-
-STAT_CHECK(){
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[32mdone\e[0m"
-  else
-    echo -e "\e[31mfail\e[0m"
-    exit 1
-  fi
-}
-
-PRINT(){
-   echo -n -e "$1\t\t"
-}
+source common.sh
 
 PRINT "Installing Nginx"
 yum install nginx -y &>>$LOG
