dc128653f9c506839297c7efe343da02480cb0ac
jinnuT <jinnuaz@hotmail.com>
2021-07-25 14:33:16 -0700

    Include & in the redirectors >> so that it can redirect actual output and error are stayed in the same file

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,8 +4,8 @@ LOG=/tmp/roboshop.log
 rm -f $LOG
 
 echo -e "Installing Nginx\t...\t\e[32mdone\e[0m"
-yum install nginx -y >>$LOG
+yum install nginx -y &>>$LOG
 echo -e "Enabling Nginx\t\t...\t\e[32mdone\e[0m"
-systemctl enable nginx >>$LOG
+systemctl enable nginx &>>$LOG
 echo -e "Starting Nginx\t\t...\t\e[32mdone\e[0m"
-systemctl start nginx >>$LOG
+systemctl start nginx &>>$LOG
