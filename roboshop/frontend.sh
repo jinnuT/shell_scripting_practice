4803c22e7bac6bc40ae26d2179bc63c1f2101cbd
jinnuT <jinnuaz@hotmail.com>
2021-07-25 14:11:47 -0700

    install - enable - start -nginx

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,5 +3,7 @@ LOG=/tmp/roboshop.log
 
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
 yum install nginx -y
-#systemctl enable nginx
-#systemctl start nginx
+echo -e "Enabling Nginx\t\t...\t\e[32mdone\e[0m"
+systemctl enable nginx
+echo -e "Starting Nginx\t\t...\t\e[32mdone\e[0m"
+systemctl start nginx
