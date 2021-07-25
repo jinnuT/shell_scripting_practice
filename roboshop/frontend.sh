92fa8d52b17ec9fd43ca0f83ae8d840ac30544d2
jinnuT <jinnuaz@hotmail.com>
2021-07-25 13:47:07 -0700

    making colar to the text done

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 LOG=/tmp/roboshop.log
 
-echo "Installing Nginx"
+echo -e "Installing Nginx\t\t...\t\e[32mdone\e[om"
 yum install nginx -y
 #systemctl enable nginx
 #systemctl start nginx
