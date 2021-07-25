787f9da15e3469c2e06baf27537d142d36f0a666
jinnuT <jinnuaz@hotmail.com>
2021-07-25 15:12:08 -0700

    Introducing some errors..& exit 1 in the start status

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -12,7 +12,7 @@ else
    exit 1
 fi
 echo -n -e "Enabling Nginx\t\t..."
-systemctl eable nginx &>>$LOG
+systemctl enable nginx &>>$LOG
 if [ $? -eq 0 ]; then
    echo -e "\e[32mdone\e[0m"
 else
@@ -20,7 +20,7 @@ else
    exit 1
 fi
 echo -n -e "Starting Nginx\t\t..."
-systemctl start nginx &>>$LOG
+systemctl star nginx &>>$LOG
 if [ $? -eq 0 ]; then
    echo -e "\e[32mdone\e[0m"
 else
