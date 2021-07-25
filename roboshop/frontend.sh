23ea3ca0595b0a35fbbfc5d03bc83d6bebe9a108
jinnuT <jinnuaz@hotmail.com>
2021-07-25 15:11:29 -0700

    Introducing some errors..& exit 1 in the enable status

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,7 +4,7 @@ LOG=/tmp/roboshop.log
 rm -f $LOG
 
 echo -n -e "Installing Nginx\t..."
-yum install ginx -y &>>$LOG
+yum install nginx -y &>>$LOG
 if [ $? -eq 0 ]; then
    echo -e "\e[32mdone\e[0m"
 else
@@ -12,7 +12,7 @@ else
    exit 1
 fi
 echo -n -e "Enabling Nginx\t\t..."
-systemctl enable nginx &>>$LOG
+systemctl eable nginx &>>$LOG
 if [ $? -eq 0 ]; then
    echo -e "\e[32mdone\e[0m"
 else
